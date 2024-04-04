6b9cce33293b420f4a56ef51eab7eb326c1ac382
avan31017 <avan31017@gmail.com>
2024-04-04 17:45:16 +0900

    Create TableViewController

---
+++ Project/CocoaTouch/CocoaTouch/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  CocoaTouch
+//
+//  Created by 조성빈 on 4/4/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ Project/CocoaTouch/CocoaTouchUITests/CocoaTouchUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  CocoaTouchUITestsLaunchTests.swift
+//  CocoaTouchUITests
+//
+//  Created by 조성빈 on 4/4/24.
+//
+
+import XCTest
+
+final class CocoaTouchUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}

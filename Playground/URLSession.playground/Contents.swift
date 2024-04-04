fe5553b0af1255f94ace4744fcf4387009cbafad
avan31017 <avan31017@gmail.com>
2024-04-04 14:27:41 +0900

    URL Session

--- Playground/URLSession.playground/Contents.swift
+++ Playground/URLSession.playground/Contents.swift
@@ -1,3 +1,24 @@
 import Foundation
+import PlaygroundSupport // import 뒤에오는 것은 '라이브러리'
+import UIKit
 
 
+//PlaygroundPage -> PlaygroundSupport에서 제공
+PlaygroundPage.current.needsIndefiniteExecution = true
+
+let config = URLSessionConfiguration.default
+let session = URLSession(configuration: config)
+
+let urlString = "https://imgs.xkcd.com/comics/api.png"
+let url = URL(string: urlString)! // 받는사람 주소
+let request = URLRequest(url: url) // 편지봉투
+
+//completionHandler -> 클로저
+let task = session.dataTask(with: request, completionHandler:  { (data, response, error) in
+    guard let imageData = data else {
+        return
+    }
+    _ = UIImage(data: imageData)
+})
+
+task.resume()

61ff29e0d69693c1fa6018d836db533239b07593
avan31017 <avan31017@gmail.com>
2024-04-05 13:28:54 +0900

    Get repo & Drawing TableView

--- Project/CocoaTouch/CocoaTouch/ReposTableViewController.swift
+++ Project/CocoaTouch/CocoaTouch/ReposTableViewController.swift
@@ -69,15 +69,18 @@ class ReposTableViewController: UITableViewController {
         
         self.title = "Seonbin"
         
-        let repo1 = Repo(name: "Test Repo 1", url: URL(string: "https://example.com/repo1"))
-        let repo2 = Repo(name: "Test Repo 2", url: URL(string: "https://example.com/repo2"))
-        
-        repos.append(contentsOf: [repo1, repo2])
-        // Uncomment the following line to preserve selection between presentations
-        // self.clearsSelectionOnViewWillAppear = false
-        
-        // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
-        // self.navigationItem.rightBarButtonItem = self.editButtonItem
+        fetchRepos(forUserName: "App-iOS5th") { [weak self] result in
+            switch result {
+            case .success(let repos):
+                self.repos = repos
+            case .failure(let error):
+                self.repos = []
+                print("에러 발생 : \(error)")
+            }
+            DispatchQueue.main.async {
+                self.tableView.reloadData()
+            }
+        }
     }
     
     // MARK: - Table view data source

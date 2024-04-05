f2504956d22f1bc7a016e0502ad6540e1d70d52c
avan31017 <avan31017@gmail.com>
2024-04-05 10:42:19 +0900

    Set TableView Cell

--- Project/CocoaTouch/CocoaTouch/ReposTableViewController.swift
+++ Project/CocoaTouch/CocoaTouch/ReposTableViewController.swift
@@ -7,83 +7,143 @@
 
 import UIKit
 
-class ReposTableViewController: UITableViewController {
+struct Repo: Codable {
+    let name: String?
+    let url: URL?
+    
+    enum CodingKeys: String, CodingKey {
+        case name = "name"
+        case url = "html_url"
+    }
+}
 
+enum FetchReposResult {
+    case success([Repo])
+    case failure(Error)
+}
+
+enum ResponseError: Error {
+    case requestUnsuccessful
+    case unexpectedResponseStructure
+}
+
+class ReposTableViewController: UITableViewController {
+    
+    internal var session = URLSession.shared
+    internal var repos = [Repo]()
+    
+    @discardableResult
+    internal func fetchRepos(forUserName username: String, completionHandler: @escaping (FetchReposResult) -> Void) -> URLSessionDataTask? {
+        let urlString = "https://api.github.com/users/\(username)/repos"
+        guard let url = URL(string: urlString) else {
+            return nil
+        }
+        var request = URLRequest(url: url)
+        request.setValue("application/vnd.github.v3+json",
+                         forHTTPHeaderField: "Accept")
+        let task = session.dataTask(with: request) { (data,
+                                                      response, error) in
+            
+            // First unwrap the optional data
+            guard let data = data else {
+                completionHandler(.failure(ResponseError.requestUnsuccessful))
+                return
+            }
+            
+            do {
+                let decoder = JSONDecoder()
+                let responseObject = try decoder.decode([Repo].self, from: data)
+                
+                completionHandler(.success(responseObject))
+            } catch {
+                completionHandler(.failure(error))
+            }
+        }
+        task.resume()
+        
+        return task
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        
+        self.title = "Seonbin"
+        
+        let repo1 = Repo(name: "Test Repo 1", url: URL(string: "https://example.com/repo1"))
+        let repo2 = Repo(name: "Test Repo 2", url: URL(string: "https://example.com/repo2"))
+        
+        repos.append(contentsOf: [repo1, repo2])
         // Uncomment the following line to preserve selection between presentations
         // self.clearsSelectionOnViewWillAppear = false
-
+        
         // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
         // self.navigationItem.rightBarButtonItem = self.editButtonItem
     }
-
+    
     // MARK: - Table view data source
-
+    
     override func numberOfSections(in tableView: UITableView) -> Int {
         // #warning Incomplete implementation, return the number of sections
-        return 0
+        return 1
     }
-
+    
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         // #warning Incomplete implementation, return the number of rows
-        return 0
+        return repos.count
     }
-
+    
+     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+     let cell = tableView.dequeueReusableCell(withIdentifier: "RepoCell", for: indexPath)
+     
+         let repo = repos[indexPath.row]
+         cell.textLabel?.text = repo.name
+     
+     return cell
+     }
+    
     /*
-    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "reuseIdentifier", for: indexPath)
-
-        // Configure the cell...
-
-        return cell
-    }
-    */
-
+     // Override to support conditional editing of the table view.
+     override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
+     // Return false if you do not want the specified item to be editable.
+     return true
+     }
+     */
+    
     /*
-    // Override to support conditional editing of the table view.
-    override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
-        // Return false if you do not want the specified item to be editable.
-        return true
-    }
-    */
-
+     // Override to support editing the table view.
+     override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
+     if editingStyle == .delete {
+     // Delete the row from the data source
+     tableView.deleteRows(at: [indexPath], with: .fade)
+     } else if editingStyle == .insert {
+     // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
+     }
+     }
+     */
+    
     /*
-    // Override to support editing the table view.
-    override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
-        if editingStyle == .delete {
-            // Delete the row from the data source
-            tableView.deleteRows(at: [indexPath], with: .fade)
-        } else if editingStyle == .insert {
-            // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
-        }    
-    }
-    */
-
+     // Override to support rearranging the table view.
+     override func tableView(_ tableView: UITableView, moveRowAt fromIndexPath: IndexPath, to: IndexPath) {
+     
+     }
+     */
+    
     /*
-    // Override to support rearranging the table view.
-    override func tableView(_ tableView: UITableView, moveRowAt fromIndexPath: IndexPath, to: IndexPath) {
-
-    }
-    */
-
+     // Override to support conditional rearranging of the table view.
+     override func tableView(_ tableView: UITableView, canMoveRowAt indexPath: IndexPath) -> Bool {
+     // Return false if you do not want the item to be re-orderable.
+     return true
+     }
+     */
+    
     /*
-    // Override to support conditional rearranging of the table view.
-    override func tableView(_ tableView: UITableView, canMoveRowAt indexPath: IndexPath) -> Bool {
-        // Return false if you do not want the item to be re-orderable.
-        return true
-    }
-    */
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
+     // MARK: - Navigation
+     
+     // In a storyboard-based application, you will often want to do a little preparation before navigation
+     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+     // Get the new view controller using segue.destination.
+     // Pass the selected object to the new view controller.
+     }
+     */
+    
 }

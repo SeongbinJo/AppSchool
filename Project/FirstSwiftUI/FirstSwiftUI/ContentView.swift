14686a540e3ab02e863bbd771799c190104c8782
avan31017 <avan31017@gmail.com>
2024-04-08 10:08:52 +0900

    Create FirstSwiftUI

---
+++ Project/FirstSwiftUI/FirstSwiftUI/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  FirstSwiftUI
+//
+//  Created by 조성빈 on 4/8/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Text("hi")
+            Text("Swift")
+        }
+    }
+}
+
+//#Preview {
+//    ContentView()
+//}

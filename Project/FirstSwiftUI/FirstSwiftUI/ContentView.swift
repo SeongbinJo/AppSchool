e07459700c72415835042652cb006575096111d6
avan31017 <avan31017@gmail.com>
2024-04-08 13:27:40 +0900

    alignment

--- Project/FirstSwiftUI/FirstSwiftUI/ContentView.swift
+++ Project/FirstSwiftUI/FirstSwiftUI/ContentView.swift
@@ -8,23 +8,42 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    @State private var toggleTF = true
+    @State private var stepValue = 0
+    @State private var slideValue: Float = 0
+    
     var body: some View {
         VStack {
-            Text("hi")
-            Text("Swift")
-            HStack {
-                Text("hi")
-                Text("Swift")
+            Text("Default padding of 16 points")
+                .padding()
+                .background(.yellow)
+            Text("Default padding of 45 points")
+                .padding(45)
+                .background(.yellow)
+            Text("Default padding of 3 points")
+                .padding(3)
+                .background(.yellow)
+            HStack(alignment: .top) {
+                Text("Top padding only")
+                    .padding(.top)
+                    .background(.yellow)
+                Text("Trailing padding only")
+                    .padding(.trailing)
+                    .background(.yellow)
+                Text("Leading padding only")
+                    .padding(.leading)
+                    .background(.yellow)
             }
-            ZStack {
-                Text("hi")
-                Text("Swift")
-            }
-            Button(action: {
-                print("clicked!")
-            }){
-                Text("This is button.")
-            }
+            Text("Bottom padding only")
+                .padding(.bottom)
+                .background(.yellow)
+            Text("Bottom padding only")
+                .padding([.top, .leading], 45)
+                .background(.yellow)
+            Text("Bottom padding only")
+                .padding([.top, .leading, .bottom], 45)
+                .background(.yellow)
         }
     }
 }

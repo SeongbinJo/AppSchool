d0f87fa92d36287a94f0cf5d472c840d1975a741
avan31017 <avan31017@gmail.com>
2024-04-18 23:16:24 +0900

    Create Calculator - Add Function & Button, Doing Color Set

---
+++ Project/DemoProject/DemoProject/StudyView/StateProperty.swift
@@ -0,0 +1,21 @@
+//
+//  StatusProperty.swift
+//  DemoProject
+//
+//  Created by 조성빈 on 4/18/24.
+//
+
+import SwiftUI
+
+struct StateProperty: View {
+    
+    @State private var wifiEnabled = true
+    
+    var body: some View {
+        Text("Hello, World!2341")
+    }
+}
+
+#Preview {
+    StateProperty()
+}

3d13595e28bcbcd144eef845c639b4c135a20146
avan31017 <avan31017@gmail.com>
2024-04-19 14:14:15 +0900

    Demoproject-2: Fetch GitHub Repos Use Async&Await

--- Project/DemoProject-2/DemoProject-2/ContentView.swift
+++ Project/DemoProject-2/DemoProject-2/ContentView.swift
@@ -7,16 +7,51 @@
 
 import SwiftUI
 
+struct Repository: Codable, Identifiable {
+    let id: Int
+    let name: String
+    let description: String?
+}
+
 struct ContentView: View {
+    
+    @State var userName: String = "SeongbinJo"
+    @State var repositories: [Repository] = []
+    
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+            TextField("Github Name : ", text: $userName)
+                .textFieldStyle(RoundedBorderTextFieldStyle())
+                .padding()
+            Button(action: {
+                Task {
+                    do {
+                        async let fetchRepositories = fetchRepos(userName: userName)
+                        repositories = try await fetchRepositories
+                    } catch {
+                        print("Error : \(error)")
+                    }
+                }
+            }) {
+                Text("레포지토리 가져오기")
+            }
+            List(repositories) { repo in
+                VStack(alignment: .leading) {
+                    Text(repo.name)
+                        .font(.headline)
+                    Text(repo.description ?? "No Description")
+                        .font(.subheadline)
+                }
+            }
         }
-        .padding()
     }
+    
+    func fetchRepos(userName: String) async throws -> [Repository] {
+        let url = URL(string: "https://api.github.com/users/\(userName)/repos")!
+        let (data, _) = try await URLSession.shared.data(from: url)
+        return try JSONDecoder().decode([Repository].self, from: data)
+    }
+    
 }
 
 #Preview {

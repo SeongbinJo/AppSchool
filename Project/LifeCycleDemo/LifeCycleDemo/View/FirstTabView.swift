c4e54ec057171adf5ce90d537d7375f9a2a168f1
avan31017 <avan31017@gmail.com>
2024-04-19 17:10:52 +0900

    Create ObservableDemo

--- Project/LifeCycleDemo/LifeCycleDemo/View/FirstTabView.swift
+++ Project/LifeCycleDemo/LifeCycleDemo/View/FirstTabView.swift
@@ -8,15 +8,30 @@
 import SwiftUI
 
 struct FirstTabView: View {
+    
+    @State var title = "View One!"
+    
     var body: some View {
-        Text("View One")
+        Text(title)
             .onAppear {
                 print("onAppear triggered")
             }
             .onDisappear {
                 print("onDisappear triggered")
+                title = "View One, Again!!"
+            }
+            .task(priority: .background) {
+                print("task triggered")
+                title = await changeTitle()
             }
     }
+    
+    
+    func changeTitle() async -> String {
+        sleep(5)
+        return "Async task complete"
+    }
+    
 }
 
 #Preview {

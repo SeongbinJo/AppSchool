9fc0a4e7f50db1822d912c443bcf26771fe5b3e1
avan31017 <avan31017@gmail.com>
2024-04-19 16:05:25 +0900

    Create LifeCycleDemo

---
+++ Project/LifeCycleDemo/LifeCycleDemo/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  LifeCycleDemo
+//
+//  Created by 조성빈 on 4/19/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Project/LifeCycleDemo/LifeCycleDemo/LifeCycleDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  LifeCycleDemoApp.swift
+//  LifeCycleDemo
+//
+//  Created by 조성빈 on 4/19/24.
+//
+
+import SwiftUI
+
+@main
+struct LifeCycleDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

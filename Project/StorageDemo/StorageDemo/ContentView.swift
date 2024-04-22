2aa8d441d0c47889347e10b361b01d94e5174469
avan31017 <avan31017@gmail.com>
2024-04-22 09:10:30 +0900

    Create StorageDemo

---
+++ Project/StorageDemo/StorageDemo/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  StorageDemo
+//
+//  Created by 조성빈 on 4/22/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}

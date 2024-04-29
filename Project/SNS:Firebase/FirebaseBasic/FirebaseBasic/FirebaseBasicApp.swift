8648b3a637a931dd4ee902daf30331f837fb6ae2
avan31017 <avan31017@gmail.com>
2024-04-29 15:06:49 +0900

    FirebaseBasic: AppDelegate

--- Project/SNS:Firebase/FirebaseBasic/FirebaseBasic/FirebaseBasicApp.swift
+++ Project/SNS:Firebase/FirebaseBasic/FirebaseBasic/FirebaseBasicApp.swift
@@ -6,9 +6,27 @@
 //
 
 import SwiftUI
+import Firebase
+import GoogleSignIn
+
+class AppDelegate: NSObject, UIApplicationDelegate {
+    func application(_ application: UIApplication,
+                     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]? = nil) -> Bool {
+        FirebaseApp.configure()
+        
+        return true
+    }
+    
+    // 외부로부터의 링크로 앱이 실행될 때 호출되는 함수
+    func application(_ app: UIApplication, open url: URL, options: [UIApplication.OpenURLOptionsKey : Any] = [:]) -> Bool {
+        GIDSignIn.sharedInstance.handle(url)
+    }
+}
 
 @main
 struct FirebaseBasicApp: App {
+    @UIApplicationDelegateAdaptor(AppDelegate.self) var delegate
+    
     var body: some Scene {
         WindowGroup {
             ContentView()

6bf8fd585a0235c1cf0921fb67450d9d27c200ab
avan31017 <avan31017@gmail.com>
2024-05-08 11:11:40 +0900

    function parameter

--- UIKit-06/Playground/MyPlayground(Fucntion, Closure).playground/Contents.swift
+++ UIKit-06/Playground/MyPlayground(Fucntion, Closure).playground/Contents.swift
@@ -21,6 +21,45 @@ func serviceCharge(forMealPrice mealCost: Int) -> Int {
 print(serviceCharge(forMealPrice: 50))
 
 
+func calculateMonthlyPayments(carPrice: Double, downPayment: Double, interestRate: Double, paymentTerm: Double) -> Double {
+    func loanAmount() -> Double {
+        return carPrice - downPayment
+    }
+ 
+    func totalInterest() -> Double {
+        return interestRate * paymentTerm
+    }
+    func numberOfMonths() -> Double {
+        return paymentTerm * 12
+    }
+    return ((loanAmount() + (loanAmount() * totalInterest() / 100)) / numberOfMonths())
+}
+calculateMonthlyPayments(carPrice: 50000, downPayment: 5000, interestRate: 3.5, paymentTerm: 7)
 
+
+// 함수를 반환하는 함수
+func makePi() -> (() -> Double) {
+    func generatePi() -> Double {
+        return 22.0 / 7.0
+    }
+    return generatePi
+}
+let pi = makePi()
+print(pi())
+
+
+func isThereAMatch(listOfNumbers: [Int], condition: (Int) -> Bool) -> Bool {
+    for item in listOfNumbers {
+        if condition(item) {
+            return true
+        }
+    }
+    return false
+}
+func oddNumber(number: Int) -> Bool {
+    return (number % 2) > 0
+}
+let numbersList = [2,4,6,7]
+isThereAMatch(listOfNumbers: numbersList, condition: oddNumber)
 
 print("--------------------------")

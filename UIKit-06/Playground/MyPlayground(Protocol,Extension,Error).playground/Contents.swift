845d38562ae0469f5646c0a49d4bf2589eb83ade
avan31017 <avan31017@gmail.com>
2024-05-09 09:31:03 +0900

    Protocol

--- UIKit-06/Playground/MyPlayground(Protocol,Extension,Error).playground/Contents.swift
+++ UIKit-06/Playground/MyPlayground(Protocol,Extension,Error).playground/Contents.swift
@@ -15,7 +15,7 @@ class Burger: CalorieCount {
     
 }
 
-class Fries: CalorieCount {
+struct Fries: CalorieCount {
     var calories: Int = 500
     
     func description() -> String {
@@ -26,6 +26,25 @@ class Fries: CalorieCount {
 enum Sauce {
     case chili
     case tomato
+    
+    var calories: Int {
+        switch self {
+        case .chili:
+            return 50
+        case .tomato:
+            return 80
+        }
+    }
+    
+    func description() -> String {
+        switch self {
+        case .chili:
+            return "chili sauce has \(calories) calories"
+        case .tomato:
+            return "tomato sauce has \(calories) calories"
+        }
+    }
 }
 
-
+var fries = Fries(calories: 200)
+fries.calories = 700

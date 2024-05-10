e083947d61b091882754feb15a02276f3e6a0931
avan31017 <avan31017@gmail.com>
2024-05-10 10:18:05 +0900

    Create Playground - TableViewBasic

---
+++ UIKit-06/Playground/TableViewBasic.playground/Contents.swift
@@ -0,0 +1,31 @@
+import UIKit
+import PlaygroundSupport
+
+class TableViewExampleController: UIViewController {
+    var tableView: UITableView?
+    var journalEntries: [[String]] = [
+    ["sun.max", "9 Apr 2023", "Nice weather today"],
+    ["cloud.rain", "10 Apr 2023", "Heavy rain today"],
+    ["cloud.sun", "11 Apr 2023", "It's cloudy out"],
+    ]
+}
+
+
+extension TableViewExampleController: UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        journalEntries.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
+        let journalEntry = journalEntries[indexPath.row]
+        var content = cell.defaultContentConfiguration()
+        content.image = UIImage(systemName: journalEntry[0])
+        content.text = journalEntry[1]
+        content.secondaryText = journalEntry[2]
+        cell.contentConfiguration = content
+        return cell
+    }
+    
+    
+}

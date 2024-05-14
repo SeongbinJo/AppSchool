4f1a4cc1fdc306066a95916a9f0cf698797f7a37
avan31017 <avan31017@gmail.com>
2024-05-14 13:06:29 +0900

    Add Map

--- PracticeProject/JRNL-CodeOnly/JRNL-CodeOnly/JournalDetailTableViewController.swift
+++ PracticeProject/JRNL-CodeOnly/JRNL-CodeOnly/JournalDetailTableViewController.swift
@@ -7,36 +7,63 @@
 
 import UIKit
 
+class CustomTableViewCell: UITableViewCell {
+    var stackView: UIStackView!
+    
+    override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
+        super.init(style: style, reuseIdentifier: reuseIdentifier)
+        setupUI()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    func setupUI() {
+        stackView = UIStackView()
+        stackView.axis = .horizontal
+        stackView.backgroundColor = .systemCyan
+        stackView.spacing = 8
+        stackView.translatesAutoresizingMaskIntoConstraints = false
+        contentView.addSubview(stackView)
+        
+        NSLayoutConstraint.activate([
+            stackView.centerXAnchor.constraint(equalTo: contentView.centerXAnchor),
+            stackView.centerYAnchor.constraint(equalTo: contentView.centerYAnchor),
+            stackView.widthAnchor.constraint(equalToConstant: 252),
+            stackView.heightAnchor.constraint(equalToConstant: 44)
+        ])
+    }
+}
+
 class JournalDetailTableViewController: UITableViewController {
     let journalEntry: JournalEntry
     
     private lazy var dateLabel: UILabel = {
-        let dateLabel = UILabel()
-        dateLabel.text = "text label"
-        dateLabel.font = UIFont.systemFont(ofSize: 17, weight: .semibold)
-        dateLabel.textAlignment = .right
-        dateLabel.translatesAutoresizingMaskIntoConstraints = false
-        return dateLabel
+        let label = UILabel()
+        label.text = "Date"
+        label.font = UIFont.systemFont(ofSize: 17, weight: .semibold)
+        label.textAlignment = .right
+        label.translatesAutoresizingMaskIntoConstraints = false
+        return label
     }()
     
     private lazy var titleLabel: UILabel = {
-        let titleLabel = UILabel()
-        titleLabel.text = "title Label"
-        titleLabel.font = UIFont.systemFont(ofSize: 17, weight: .semibold)
-        titleLabel.textAlignment = .left
-        titleLabel.translatesAutoresizingMaskIntoConstraints = false
-        return titleLabel
+        let label = UILabel()
+        label.font = UIFont.systemFont(ofSize: 17, weight: .semibold)
+        label.translatesAutoresizingMaskIntoConstraints = false
+        return label
     }()
     
     private lazy var bodyTextView: UITextView = {
-        let bodyTextView = UITextView()
-        bodyTextView.isEditable = false
-        bodyTextView.isSelectable = false
-        bodyTextView.backgroundColor = .systemBackground
-        bodyTextView.textColor = .label
-        bodyTextView.font = UIFont.systemFont(ofSize: 14)
-        bodyTextView.translatesAutoresizingMaskIntoConstraints = false
-        return bodyTextView
+        let textView = UITextView()
+        textView.isEditable = false
+        textView.isSelectable = false
+        textView.backgroundColor = .systemBackground
+        textView.textColor = .label
+        textView.font = UIFont.systemFont(ofSize: 14)
+        textView.translatesAutoresizingMaskIntoConstraints = false
+        return textView
     }()
     
     private lazy var imageView: UIImageView = {
@@ -48,16 +75,16 @@ class JournalDetailTableViewController: UITableViewController {
     }()
     
     private lazy var mapView: UIImageView = {
-        let mapView = UIImageView()
-        mapView.image = UIImage(systemName: "map.fill")
-        mapView.contentMode = .scaleAspectFit
-        mapView.translatesAutoresizingMaskIntoConstraints = false
-        return mapView
+        let imageView = UIImageView()
+        imageView.image = UIImage(systemName: "map")
+        imageView.contentMode = .scaleAspectFit
+        imageView.translatesAutoresizingMaskIntoConstraints = false
+        return imageView
     }()
     
     init(journalEntry: JournalEntry) {
         self.journalEntry = journalEntry
-        super.init(nibName: nil, bundle: nil)
+        super.init(nibName: nil, bundle:nil)
     }
     
     required init?(coder: NSCoder) {
@@ -66,72 +93,89 @@ class JournalDetailTableViewController: UITableViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        
         tableView.register(UITableViewCell.self, forCellReuseIdentifier: "cell")
-        navigationItem.title = "Journal Detail View"
+        tableView.register(CustomTableViewCell.self, forCellReuseIdentifier: "customCell")
+        navigationItem.title = "Detail"
     }
-    
+
     // MARK: - Table view data source
-    
+
     override func numberOfSections(in tableView: UITableView) -> Int {
         // #warning Incomplete implementation, return the number of sections
         return 1
     }
-    
+
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         // #warning Incomplete implementation, return the number of rows
         return 7
     }
-    
-    
+
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
-        let marginGuide = cell.layoutMarginsGuide
+
         switch indexPath.row {
         case 0:
+            let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
             cell.contentView.addSubview(dateLabel)
-//            let marginGuide = cell.layoutMarginsGuide
+            dateLabel.text = journalEntry.date.formatted(.dateTime.year().month().day())
+
+            let marginGuide = cell.contentView.layoutMarginsGuide
             NSLayoutConstraint.activate([
-                dateLabel.centerYAnchor.constraint(equalTo: marginGuide.centerYAnchor),
-                dateLabel.leadingAnchor.constraint(equalTo: marginGuide.leadingAnchor, constant: 16),
-                dateLabel.trailingAnchor.constraint(equalTo: marginGuide.trailingAnchor, constant: -16),
+                dateLabel.centerYAnchor.constraint(equalTo: cell.contentView.centerYAnchor),
+                dateLabel.leadingAnchor.constraint(equalTo: marginGuide.leadingAnchor),
+                dateLabel.trailingAnchor.constraint(equalTo: marginGuide.trailingAnchor)
             ])
+            return cell
+        case 1:
+            let cell = tableView.dequeueReusableCell(withIdentifier: "customCell", for: indexPath) as! CustomTableViewCell
+            return cell
         case 2:
-//            cell.contentView.addSubview(titleLabel)
-            cell.addSubview(titleLabel)
+            let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
+            cell.contentView.addSubview(titleLabel)
+            titleLabel.text = journalEntry.title
+            let marginGuide = cell.contentView.layoutMarginsGuide
             NSLayoutConstraint.activate([
-                titleLabel.centerYAnchor.constraint(equalTo: marginGuide.centerYAnchor),
-                titleLabel.leadingAnchor.constraint(equalTo: marginGuide.leadingAnchor, constant: 16),
-                titleLabel.trailingAnchor.constraint(equalTo: marginGuide.trailingAnchor, constant: -16),
+                titleLabel.centerYAnchor.constraint(equalTo: cell.contentView.centerYAnchor),
+                titleLabel.leadingAnchor.constraint(equalTo: marginGuide.leadingAnchor),
+                titleLabel.trailingAnchor.constraint(equalTo: marginGuide.trailingAnchor)
             ])
+            return cell
         case 3:
+            let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
             cell.contentView.addSubview(bodyTextView)
-            bodyTextView.text = journalEntry.title
+            bodyTextView.text = journalEntry.body
+            let marginGuide = cell.contentView.layoutMarginsGuide
             NSLayoutConstraint.activate([
                 bodyTextView.topAnchor.constraint(equalTo: marginGuide.topAnchor),
                 bodyTextView.bottomAnchor.constraint(equalTo: marginGuide.bottomAnchor),
                 bodyTextView.leadingAnchor.constraint(equalTo: marginGuide.leadingAnchor),
-                bodyTextView.trailingAnchor.constraint(equalTo: marginGuide.trailingAnchor),
+                bodyTextView.trailingAnchor.constraint(equalTo: marginGuide.trailingAnchor)
             ])
+            return cell
         case 4:
+            let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
             cell.contentView.addSubview(imageView)
+            imageView.image = journalEntry.photo
             NSLayoutConstraint.activate([
-                imageView.centerXAnchor.constraint(equalTo: marginGuide.centerXAnchor),
-                imageView.centerYAnchor.constraint(equalTo: marginGuide.centerYAnchor),
+                imageView.centerXAnchor.constraint(equalTo: cell.contentView.centerXAnchor),
+                imageView.centerYAnchor.constraint(equalTo: cell.contentView.centerYAnchor),
                 imageView.widthAnchor.constraint(equalToConstant: 300),
-                imageView.heightAnchor.constraint(equalToConstant: 300),
+                imageView.heightAnchor.constraint(equalToConstant: 300)
             ])
+            return cell
         case 5:
+            let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
             cell.contentView.addSubview(mapView)
             NSLayoutConstraint.activate([
-                mapView.centerXAnchor.constraint(equalTo: marginGuide.centerXAnchor),
-                mapView.centerYAnchor.constraint(equalTo: marginGuide.centerYAnchor),
+                mapView.centerXAnchor.constraint(equalTo: cell.contentView.centerXAnchor),
+                mapView.centerYAnchor.constraint(equalTo: cell.contentView.centerYAnchor),
                 mapView.widthAnchor.constraint(equalToConstant: 300),
-                mapView.heightAnchor.constraint(equalToConstant: 300),
+                mapView.heightAnchor.constraint(equalToConstant: 300)
             ])
-        default: break
+            return cell
+        default:
+            return tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
         }
-        
-        return cell
     }
     
     override func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
@@ -143,50 +187,50 @@ class JournalDetailTableViewController: UITableViewController {
         default: return 44.5
         }
     }
-    
+
     /*
-     // Override to support conditional editing of the table view.
-     override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
-     // Return false if you do not want the specified item to be editable.
-     return true
-     }
-     */
-    
+    // Override to support conditional editing of the table view.
+    override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
+        // Return false if you do not want the specified item to be editable.
+        return true
+    }
+    */
+
     /*
-     // Override to support editing the table view.
-     override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
-     if editingStyle == .delete {
-     // Delete the row from the data source
-     tableView.deleteRows(at: [indexPath], with: .fade)
-     } else if editingStyle == .insert {
-     // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
-     }
-     }
-     */
-    
+    // Override to support editing the table view.
+    override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
+        if editingStyle == .delete {
+            // Delete the row from the data source
+            tableView.deleteRows(at: [indexPath], with: .fade)
+        } else if editingStyle == .insert {
+            // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
+        }
+    }
+    */
+
     /*
-     // Override to support rearranging the table view.
-     override func tableView(_ tableView: UITableView, moveRowAt fromIndexPath: IndexPath, to: IndexPath) {
-     
-     }
-     */
-    
+    // Override to support rearranging the table view.
+    override func tableView(_ tableView: UITableView, moveRowAt fromIndexPath: IndexPath, to: IndexPath) {
+
+    }
+    */
+
     /*
-     // Override to support conditional rearranging of the table view.
-     override func tableView(_ tableView: UITableView, canMoveRowAt indexPath: IndexPath) -> Bool {
-     // Return false if you do not want the item to be re-orderable.
-     return true
-     }
-     */
-    
+    // Override to support conditional rearranging of the table view.
+    override func tableView(_ tableView: UITableView, canMoveRowAt indexPath: IndexPath) -> Bool {
+        // Return false if you do not want the item to be re-orderable.
+        return true
+    }
+    */
+
     /*
-     // MARK: - Navigation
-     
-     // In a storyboard-based application, you will often want to do a little preparation before navigation
-     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-     // Get the new view controller using segue.destination.
-     // Pass the selected object to the new view controller.
-     }
-     */
-    
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
 }
--- PracticeProject/JRNL-CodeOnly/JRNL-CodeOnly/MapViewController.swift
+++ PracticeProject/JRNL-CodeOnly/JRNL-CodeOnly/MapViewController.swift
@@ -6,13 +6,30 @@
 //
 
 import UIKit
+import MapKit
 
 class MapViewController: UIViewController {
-
+    private lazy var mapView: MKMapView = {
+       let mapView = MKMapView()
+        mapView.translatesAutoresizingMaskIntoConstraints = false
+        return mapView
+    }()
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        view.backgroundColor = .white
+        navigationItem.title = "Map"
 
-        // Do any additional setup after loading the view.
+        view.addSubview(mapView)
+        
+        let safeArea = view.safeAreaLayoutGuide
+        NSLayoutConstraint.activate([
+            mapView.topAnchor.constraint(equalTo: safeArea.topAnchor),
+            mapView.bottomAnchor.constraint(equalTo: safeArea.bottomAnchor),
+            mapView.leadingAnchor.constraint(equalTo: safeArea.leadingAnchor),
+            mapView.trailingAnchor.constraint(equalTo: safeArea.trailingAnchor)
+        ])
     }
     
 

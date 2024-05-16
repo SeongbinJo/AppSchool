ef679a0d7745dc03404e0240d49c9d64ce6f5216
avan31017 <avan31017@gmail.com>
2024-05-16 09:28:08 +0900

    Create SharedData

--- PracticeProject/JRNL/JRNL/Entry Detail Screen/JournalEntryDetailTableViewController.swift
+++ PracticeProject/JRNL/JRNL/Entry Detail Screen/JournalEntryDetailTableViewController.swift
@@ -6,12 +6,14 @@
 //
 
 import UIKit
+import MapKit
 
 class JournalEntryDetailTableViewController: UITableViewController {
     @IBOutlet var dateLabel: UILabel!
     @IBOutlet var titleLabel: UILabel!
     @IBOutlet var bodyTextView: UITextView!
     @IBOutlet var photoImageView: UIImageView!
+    @IBOutlet var mapImageView: UIImageView!
     
     var selectedJournalEntry: JournalEntry?
     
@@ -21,72 +23,29 @@ class JournalEntryDetailTableViewController: UITableViewController {
         titleLabel.text = selectedJournalEntry?.title
         bodyTextView.text = selectedJournalEntry?.body
         photoImageView.image = selectedJournalEntry?.photo
-        // Uncomment the following line to preserve selection between presentations
-        // self.clearsSelectionOnViewWillAppear = false
-
-        // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
-        // self.navigationItem.rightBarButtonItem = self.editButtonItem
+        
+        getMapSnapshot()
     }
-
-    // MARK: - Table view data source
-
-//    override func numberOfSections(in tableView: UITableView) -> Int {
-//        // #warning Incomplete implementation, return the number of sections
-//        return 1
-//    }
-//
-//    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-//        // #warning Incomplete implementation, return the number of rows
-//        return 7
-//    }
-
-    /*
-    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "reuseIdentifier", for: indexPath)
-
-        // Configure the cell...
-
-        return cell
-    }
-    */
-
-    /*
-    // Override to support conditional editing of the table view.
-    override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
-        // Return false if you do not want the specified item to be editable.
-        return true
-    }
-    */
-
-    /*
-    // Override to support editing the table view.
-    override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
-        if editingStyle == .delete {
-            // Delete the row from the data source
-            tableView.deleteRows(at: [indexPath], with: .fade)
-        } else if editingStyle == .insert {
-            // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
-        }    
-    }
-    */
-
-    /*
-    // Override to support rearranging the table view.
-    override func tableView(_ tableView: UITableView, moveRowAt fromIndexPath: IndexPath, to: IndexPath) {
-
-    }
-    */
-
-    /*
-    // Override to support conditional rearranging of the table view.
-    override func tableView(_ tableView: UITableView, canMoveRowAt indexPath: IndexPath) -> Bool {
-        // Return false if you do not want the item to be re-orderable.
-        return true
-    }
-    */
     
-    // MARK: - Navigation
-//    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-//    }
+    //MARK: - Private Methods
+    // 맵의 스크린샷을 찍어 옴
+    private func getMapSnapshot() {
+        if let lat = selectedJournalEntry?.latitude, let long = selectedJournalEntry?.longitude {
+            let options = MKMapSnapshotter.Options()
+            options.region = MKCoordinateRegion(center: CLLocationCoordinate2D(latitude: lat, longitude: long), span: MKCoordinateSpan(latitudeDelta: 0.01, longitudeDelta: 0.01))
+            options.size = CGSize(width: 300, height: 300)
+            options.preferredConfiguration = MKStandardMapConfiguration()
+            let snapShotter = MKMapSnapshotter(options: options)
+            snapShotter.start { snapShot, error in
+                if let snapshot = snapShot {
+                    self.mapImageView.image = snapshot.image
+                }else if let error = error {
+                    print("Map snapShot error : \(error.localizedDescription)")
+                }
+            }
+        } else {
+            self.mapImageView.image = UIImage(systemName: "map")
+        }
+    }
 
 }
---
+++ PracticeProject/JRNL/JRNL/Model/SharedData.swift
@@ -0,0 +1,38 @@
+//
+//  SharedData.swift
+//  JRNL
+//
+//  Created by 조성빈 on 5/16/24.
+//
+
+import UIKit
+
+class SharedData {
+    static let shared = SharedData()
+    private var journalEntries: [JournalEntry]
+    
+    private init() {
+        journalEntries = []
+    }
+    
+    func numberOfJournalEntries() -> Int {
+        journalEntries.count
+    }
+    
+    func getJournalEntry(index: Int) -> JournalEntry {
+        return journalEntries[index]
+    }
+    
+    func getAllJournalEntries(index: Int) -> [JournalEntry] {
+        let readOnlyJournalEntries = journalEntries
+        return readOnlyJournalEntries
+    }
+    
+    func addJournalEntry(newJournalEntry: JournalEntry) {
+        journalEntries.append(newJournalEntry)
+    }
+    
+    func removeJournalEntry(index: Int) {
+        journalEntries.remove(at: index)
+    }
+}

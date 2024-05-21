cf586ef8e1d5224ee0bfc5fc8b74a5302f13533a
avan31017 <avan31017@gmail.com>
2024-05-21 15:31:22 +0900

    Using WebView with WebKit

--- PracticeProject/UIKit-BasicPractice/LinkWithUIKit/LinkWithUIKit/Link/LinkViewController.swift
+++ PracticeProject/UIKit-BasicPractice/LinkWithUIKit/LinkWithUIKit/Link/LinkViewController.swift
@@ -6,6 +6,7 @@
 //
 
 import UIKit
+import WebKit
 
 class LinkViewController: UIViewController {
     let linkButton: UIButton = {
@@ -14,6 +15,31 @@ class LinkViewController: UIViewController {
         config.title = "Apple"
         button.configuration = config
         button.titleLabel?.font = UIFont.systemFont(ofSize: 18)
+        
+        button.addAction(UIAction { _ in
+            if let url = URL(string: "https://www.apple.com") {
+                UIApplication.shared.open(url)
+            }
+        }, for: .touchUpInside)
+        
+        button.translatesAutoresizingMaskIntoConstraints = false
+        return button
+    }()
+    
+    private lazy var webView: WKWebView = {
+        let webView = WKWebView(frame: .zero)
+        webView.translatesAutoresizingMaskIntoConstraints = false
+        webView.isHidden = true
+        return webView
+    }()
+    
+    let webViewButton: UIButton = {
+        let button = UIButton(type: .system)
+        var config = UIButton.Configuration.filled()
+        config.title = "naver"
+        button.configuration = config
+        button.titleLabel?.font = UIFont.systemFont(ofSize: 18)
+        
         button.translatesAutoresizingMaskIntoConstraints = false
         return button
     }()
@@ -21,18 +47,34 @@ class LinkViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        linkButton.addAction(UIAction { _ in
-            if let url = URL(string: "https://www.apple.com") {
-                UIApplication.shared.open(url)
-            }
-        }, for: .touchUpInside)
-        
         view.addSubview(linkButton)
+        view.addSubview(webView)
+        view.addSubview(webViewButton)
         
         NSLayoutConstraint.activate([
             linkButton.centerXAnchor.constraint(equalTo: view.centerXAnchor),
-            linkButton.centerYAnchor.constraint(equalTo: view.centerYAnchor),
+            linkButton.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor),
+            
+            webViewButton.topAnchor.constraint(equalTo: linkButton.topAnchor),
+            webViewButton.leadingAnchor.constraint(equalTo: linkButton.trailingAnchor, constant: 20),
+            
+            webView.topAnchor.constraint(equalTo: linkButton.bottomAnchor, constant: 20),
+            webView.leadingAnchor.constraint(equalTo: view.leadingAnchor),
+            webView.trailingAnchor.constraint(equalTo: view.trailingAnchor),
+            webView.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor),
         ])
+        
+        webViewButton.addAction(UIAction { [weak self] _ in
+            if let url = URL(string: "https://www.naver.com") {
+                self?.openWebView(url: url)
+            }
+        }, for: .touchUpInside)
+    }
+    
+    func openWebView(url: URL) {
+        let request = URLRequest(url: url)
+        webView.load(request)
+        webView.isHidden = false
     }
 
 }

b8787f041c05859a470d4485f06e59208aa311b2
avan31017 <avan31017@gmail.com>
2024-05-22 14:43:55 +0900

    Table with section(스타일 추가)

--- UIKit-BasicPractice/ListWithUIKit/ListWithUIKit/ViewController.swift
+++ UIKit-BasicPractice/ListWithUIKit/ListWithUIKit/ViewController.swift
@@ -7,13 +7,27 @@
 
 import UIKit
 
+struct Animal {
+    let name: String
+}
+
+struct AnimalCategory {
+    let category: String
+    let animals: [Animal]
+}
+
 class ViewController: UIViewController {
+    let categories = [
+        AnimalCategory(category: "파충류", animals: [Animal(name: "크레스티드 게코 도마뱀"), Animal(name: "레오파드 게코 도마뱀"), Animal(name: "토게이 게코 도마뱀")]),
+        AnimalCategory(category: "포유류", animals: [Animal(name: "강아지"), Animal(name: "고양이"), Animal(name: "원숭이")]),
+    ]
+    
     let items = ["고양이", "강아지", "조류", "파충류", "어류"]
     
     private lazy var tableView: UITableView = {
-        let tableView = UITableView(frame: view.bounds)
+        let tableView = UITableView(frame: view.bounds, style: .insetGrouped)
         tableView.dataSource = self
-    
+        tableView.register(UITableViewCell.self, forCellReuseIdentifier: "cell")
         return tableView
     }()
 
@@ -31,14 +45,22 @@ class ViewController: UIViewController {
 }
 
 extension ViewController: UITableViewDelegate, UITableViewDataSource {
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return categories.count
+    }
+    
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        items.count
+        return categories[section].animals.count
+    }
+    
+    func tableView(_ tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
+        return categories[section].category
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "cell") ?? UITableViewCell(style: .default, reuseIdentifier: "cell")
         var config = cell.defaultContentConfiguration()
-        config.text = items[indexPath.row]
+        config.text = categories[indexPath.section].animals[indexPath.row].name
         cell.contentConfiguration = config
         return cell
     }

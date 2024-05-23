623230df7d6433addcd8c32c3dc871ed1de195c7
avan31017 <avan31017@gmail.com>
2024-05-23 09:51:07 +0900

    이전 커밋:  기존 셀 사용(수정) & 현재 커밋: 커스텀 셀 적용

--- UIKit-BasicPractice/ListWithUIKit/ListWithUIKit/CustomCellViewController.swift
+++ UIKit-BasicPractice/ListWithUIKit/ListWithUIKit/CustomCellViewController.swift
@@ -12,6 +12,35 @@ struct AnimalStruct {
     let image: UIImage
 }
 
+class CustomCell: UITableViewCell {
+    let animalImageView = UIImageView()
+    let nameLabel = UILabel()
+    
+    override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
+        super.init(style: style, reuseIdentifier: reuseIdentifier)
+        animalImageView.contentMode = .scaleAspectFit
+        animalImageView.translatesAutoresizingMaskIntoConstraints = false
+        nameLabel.translatesAutoresizingMaskIntoConstraints = false
+        
+        contentView.addSubview(animalImageView)
+        contentView.addSubview(nameLabel)
+        
+        NSLayoutConstraint.activate([
+            animalImageView.leadingAnchor.constraint(equalTo: contentView.leadingAnchor, constant: 16),
+            animalImageView.centerYAnchor.constraint(equalTo: contentView.centerYAnchor),
+            animalImageView.widthAnchor.constraint(equalToConstant: 50),
+            animalImageView.heightAnchor.constraint(equalToConstant: 50),
+            
+            nameLabel.leadingAnchor.constraint(equalTo: animalImageView.trailingAnchor, constant: 16),
+            nameLabel.centerYAnchor.constraint(equalTo: contentView.centerYAnchor)
+        ])
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+}
+
 class CustomCellViewController: UIViewController {
     let animals = [
         AnimalStruct(name: "강아지", image: UIImage(systemName: "dog")!),
@@ -22,8 +51,7 @@ class CustomCellViewController: UIViewController {
     private lazy var tableView: UITableView = {
         let tableView = UITableView(frame: view.bounds)
         tableView.dataSource = self
-        tableView.register(UITableViewCell.self, forCellReuseIdentifier: "cell")
-        tableView.translatesAutoresizingMaskIntoConstraints = false
+        tableView.register(CustomCell.self, forCellReuseIdentifier: "cell")
         return tableView
     }()
 
@@ -41,11 +69,14 @@ extension CustomCellViewController: UITableViewDataSource {
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
-        var config = cell.defaultContentConfiguration()
-        config.text = animals[indexPath.row].name
-        config.image = animals[indexPath.row].image
-        cell.contentConfiguration = config
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath) as! CustomCell
+//        var config = cell.defaultContentConfiguration()
+//        config.text = animals[indexPath.row].name
+//        config.image = animals[indexPath.row].image
+//        cell.contentConfiguration = config
+        cell.animalImageView.image = animals[indexPath.row].image
+        cell.nameLabel.text = animals[indexPath.row].name
+        
         return cell
     }
     

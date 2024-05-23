dc912a3532b0e1506b6b1b85808d2dd60f566f37
avan31017 <avan31017@gmail.com>
2024-05-23 17:08:02 +0900

    네비게이션 바 safeArea까지 색 변경

--- UIKit-BasicPractice/NavigationWithUIKit/NavigationWithUIKit/ViewController.swift
+++ UIKit-BasicPractice/NavigationWithUIKit/NavigationWithUIKit/ViewController.swift
@@ -13,17 +13,36 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         self.navigationItem.title = "네비게이션 타이틀"
         
+        let appearance = UINavigationBarAppearance()
+        appearance.titleTextAttributes = [NSAttributedString.Key.foregroundColor: UIColor.white]
+        appearance.backgroundColor = .systemCyan
+        
         self.navigationController?.navigationBar.barStyle = .default
         self.navigationController?.navigationBar.isTranslucent = false
-        self.navigationController?.navigationBar.tintColor = .white
-        self.navigationController?.navigationBar.barTintColor = .systemCyan
-        self.navigationController?.navigationBar.titleTextAttributes = [NSAttributedString.Key.foregroundColor: UIColor.red]
+        self.navigationController?.navigationBar.scrollEdgeAppearance = appearance
         
         let leftButton = UIBarButtonItem(barButtonSystemItem: .add, target: self, action: #selector(leftButtonTapped))
         let rightButton = UIBarButtonItem(title: "완료(오른쪽)", style: .done, target: self, action: #selector(rightButtonTapped))
         let extraButton = UIBarButtonItem(barButtonSystemItem: .action, target: self, action: #selector(extraButtonTapped))
         self.navigationItem.leftBarButtonItems = [leftButton]
         self.navigationItem.rightBarButtonItems = [rightButton, extraButton]
+        
+        let button = UIButton(type: .system)
+        button.setTitle("다음 화면으로", for: .normal)
+        button.addAction(UIAction { [weak self] _ in
+            print("다음 화면으로 => 탭됨.")
+            let nextViewController = UIViewController()
+            nextViewController.view.backgroundColor = .white
+            self?.show(nextViewController, sender: nil)
+        }, for: .touchUpInside)
+        button.frame = CGRect(x: 100, y: 100, width: 200, height: 50)
+        button.translatesAutoresizingMaskIntoConstraints = false
+        
+        view.addSubview(button)
+        
+        NSLayoutConstraint.activate([
+            
+        ])
     }
 
     @objc func leftButtonTapped() {

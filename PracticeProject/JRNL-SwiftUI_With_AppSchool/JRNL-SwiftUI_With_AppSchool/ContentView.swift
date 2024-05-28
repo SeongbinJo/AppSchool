85c739d7a5a7dbadc53d44d25500babd777748bd
avan31017 <avan31017@gmail.com>
2024-05-28 17:03:13 +0900

    Create JRNL_SwiftUI_With_AppSchoolApp

---
+++ PracticeProject/JRNL-SwiftUI_With_AppSchool/JRNL-SwiftUI_With_AppSchool/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  JRNL-SwiftUI_With_AppSchool
+//
+//  Created by 조성빈 on 5/28/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ PracticeProject/JRNL-SwiftUI_With_AppSchool/JRNL-SwiftUI_With_AppSchool/JRNL_SwiftUI_With_AppSchoolApp.swift
@@ -0,0 +1,17 @@
+//
+//  JRNL_SwiftUI_With_AppSchoolApp.swift
+//  JRNL-SwiftUI_With_AppSchool
+//
+//  Created by 조성빈 on 5/28/24.
+//
+
+import SwiftUI
+
+@main
+struct JRNL_SwiftUI_With_AppSchoolApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

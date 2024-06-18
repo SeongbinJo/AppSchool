663e9094d1c86efc41ddba252adac96959fc116c
avan31017 <avan31017@gmail.com>
2024-06-18 11:34:15 +0900

    Create (Playground)StartCombine & Using Just(), publisher, sink

---
+++ StartCombine.playground/Contents.swift
@@ -0,0 +1,22 @@
+import Foundation
+import Combine
+
+Just(42)
+    .sink { value in // 전달받은 값으로 무언가 행동을 취한다. -> sink
+        print("Received \(value)")
+    }
+
+["페퍼로니", "버섯", "양파", "치즈"].publisher // 값을 흘려보낸다. -> publisher
+    .sink { topping in
+        print("\(topping) is on my pizza.")
+    }
+
+"Combine".publisher // 문자열 자체를 흘려보내면 Char로 쪼개져 흐른다.
+    .sink { value in
+        print("\(value), Hello")
+    }
+
+Just("Combine") // 문자열을 Just를 사용해서 흘려보내면 문자열 덩어리가 흐른다.
+    .sink { value in
+        print("\(value), Hello")
+    }

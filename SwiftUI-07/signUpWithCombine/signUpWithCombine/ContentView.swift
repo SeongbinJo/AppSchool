c9483a1c035cd40b666b54e9f2e1d39533783c31
avan31017 <avan31017@gmail.com>
2024-06-18 16:33:01 +0900

    Reactive의 또 다른 방법

--- SwiftUI-07/signUpWithCombine/signUpWithCombine/ContentView.swift
+++ SwiftUI-07/signUpWithCombine/signUpWithCombine/ContentView.swift
@@ -6,6 +6,7 @@
 //
 
 import SwiftUI
+import Combine
 
 class SignUpFormViewModel: ObservableObject {
     @Published var username: String = ""
@@ -16,12 +17,21 @@ class SignUpFormViewModel: ObservableObject {
     @Published var passwordMessage: String = ""
     @Published var isValid: Bool = false
     
+    // Never = '나는 에러 절대 안낼거야!' 라는 확증
+    private lazy var isUsernameLengthValidPublisher: AnyPublisher<Bool, Never> = {
+        $username.map { $0.count >= 3 }.eraseToAnyPublisher()
+    }()
+    
     init() {
+        // Reactive 패턴
         // @Published 이므로 $을 붙이고 Operator와 Subscriber를 사용할 수 있다.
-        $username.map { $0.count >= 3 }
-            .assign(to: &$isValid) // 3이상일 경우 isValid = true
-        $username.map { $0.count >= 3 ? "" : "Username must be at least 3 characters."}
-            .assign(to: &$usernameMessage)
+//        $username.map { $0.count >= 3 }
+//            .assign(to: &$isValid) // 3이상일 경우 isValid = true
+//        $username.map { $0.count >= 3 ? "" : "Username must be at least 3 characters."}
+//            .assign(to: &$usernameMessage)
+        // 위와 동일
+        isUsernameLengthValidPublisher.assign(to: &$isValid)
+        isUsernameLengthValidPublisher.map { $0 ? "" : "Username must be at least 3 characters."}.assign(to: &$usernameMessage)
     }
 }
 

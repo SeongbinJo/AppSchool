8c2b0fbc84d4981719355980e6823aca4a852e2c
avan31017 <avan31017@gmail.com>
2024-06-18 16:18:56 +0900

    setup view

--- SwiftUI-07/signUpWithCombine/signUpWithCombine/ContentView.swift
+++ SwiftUI-07/signUpWithCombine/signUpWithCombine/ContentView.swift
@@ -7,18 +7,48 @@
 
 import SwiftUI
 
-struct ContentView: View {
-    var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
-        }
-        .padding()
-    }
+class SignUpFormViewModel: ObservableObject {
+    @Published var username: String = ""
+    @Published var password: String = ""
+    @Published var passwordConfirmation: String = ""
+    
+    @Published var usernameMessage: String = ""
+    @Published var passwordMessage: String = ""
+    @Published var isValid: Bool = false
 }
 
-#Preview {
-    ContentView()
+struct ContentView: View {
+    @StateObject var viewModel = SignUpFormViewModel()
+    
+    var body: some View {
+        Form {
+            Section(content: {
+                TextField("Username", text: $viewModel.username)
+                    .textInputAutocapitalization(.none)
+                    .autocorrectionDisabled()
+            }, footer: {
+                Text(viewModel.usernameMessage)
+                    .foregroundStyle(.red)
+            })
+            
+            Section(content: {
+                SecureField("Password", text: $viewModel.password)
+                SecureField("Repeat password", text: $viewModel.passwordConfirmation)
+            }, footer: {
+                Text(viewModel.passwordMessage)
+                    .foregroundStyle(.red)
+            })
+            
+            Section {
+                Button("Sign up") {
+                    print("Signing up as \(viewModel.username)")
+                }
+                .disabled(!viewModel.isValid)
+            }
+        }
+    }
 }
+    
+    #Preview {
+        ContentView()
+    }

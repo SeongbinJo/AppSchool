b479eceef76dc3f351f4f4c7c8f96ecd99e0c6bd
avan31017 <avan31017@gmail.com>
2024-06-20 11:45:13 +0900

    isUsernameAvailablePublisher 추가

--- SwiftUI-07/Practice_SignUpCombine_With_Server/Practice_SignUpCombine_With_Server/SignUpFormViewModel.swift
+++ SwiftUI-07/Practice_SignUpCombine_With_Server/Practice_SignUpCombine_With_Server/SignUpFormViewModel.swift
@@ -9,10 +9,26 @@ import Foundation
 import Combine
 
 class SignUpFormViewModel: ObservableObject {
+    typealias Available = Result<Bool, Error>
+    
     @Published var username: String = ""
     @Published var usernameMessage: String = ""
     
     @Published var isValid: Bool = false
     
     @Published var showUpdateDialog: Bool = false
+    
+    private var authenticationService = AuthenticationService()
+    
+    private lazy var isUsernameAvailablePublisher: AnyPublisher<Available, Never> = {
+        $username
+            .debounce(for: 0.5, scheduler: RunLoop.main)
+            .removeDuplicates()
+            .flatMap { username -> AnyPublisher<Available, Never> in
+                self.authenticationService.checkUserNameAvailablePublisher(userName: username)
+                    .asResult()
+            }
+            .share() //서버코드가 들어가있기 때문에 모든 퍼블리셔가 같은 만드는 것 보다 결과를 공유하는게 낫다.
+            .eraseToAnyPublisher()
+    }()
 }

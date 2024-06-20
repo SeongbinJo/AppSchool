b613ecff1a1502c00a060633660885e95ff5ada3
avan31017 <avan31017@gmail.com>
2024-06-20 11:12:23 +0900

    AuthenticationService 추가

--- SwiftUI-07/Practice_SignUpCombine_With_Server/Practice_SignUpCombine_With_Server/AuthenticationService.swift
+++ SwiftUI-07/Practice_SignUpCombine_With_Server/Practice_SignUpCombine_With_Server/AuthenticationService.swift
@@ -11,6 +11,13 @@ import Combine
 
 struct AuthenticationService {
     func checkUserNameAvailablePublisher(userName: String) -> AnyPublisher<Bool, Error> {
-        return Fail(error: APIError.invalidResponse).eraseToAnyPublisher() // 임시 퍼블리셔 반환
+        guard let url = URL(string: "http://127.0.0.1:8080/isUserNameAvailable?userName=\(userName)") else {
+            return Fail(error: APIError.invalidResponse).eraseToAnyPublisher() // 임시 퍼블리셔 반환
+        }
+        return URLSession.shared.dataTaskPublisher(for: url)
+            .map(\.data)
+            .decode(type: UserNameAvailableMessage.self, decoder: JSONDecoder())
+            .map(\.isAvailable)
+            .eraseToAnyPublisher()
     }
 }

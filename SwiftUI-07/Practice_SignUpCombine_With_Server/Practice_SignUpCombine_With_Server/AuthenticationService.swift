62fc6fe6ef8548c3e7b15b2b13eee691a81fbf43
avan31017 <avan31017@gmail.com>
2024-06-20 14:30:29 +0900

    http 상태코드에 따른 에러처리

--- SwiftUI-07/Practice_SignUpCombine_With_Server/Practice_SignUpCombine_With_Server/AuthenticationService.swift
+++ SwiftUI-07/Practice_SignUpCombine_With_Server/Practice_SignUpCombine_With_Server/AuthenticationService.swift
@@ -12,9 +12,36 @@ import Combine
 struct AuthenticationService {
     func checkUserNameAvailablePublisher(userName: String) -> AnyPublisher<Bool, Error> {
         guard let url = URL(string: "http://127.0.0.1:8080/isUserNameAvailable?userName=\(userName)") else {
-            return Fail(error: APIError.invalidResponse).eraseToAnyPublisher() // 임시 퍼블리셔 반환
+            return Fail(error: APIError.invalidRequestError("URL Invalid")).eraseToAnyPublisher() // 임시 퍼블리셔 반환
         }
-        return URLSession.shared.dataTaskPublisher(for: url)
+        let dataTaskPublisher = URLSession.shared.dataTaskPublisher(for: url)
+            .mapError { error -> Error in
+                return APIError.transportError(error)
+            }
+            .tryMap { (data, response) -> (data: Data, response: URLResponse) in
+                print("Received response from server, no checking status code")
+                
+                guard let urlResponse = response as? HTTPURLResponse else {
+                    throw APIError.invalidResponse
+                }
+                
+                if (200..<300) ~= urlResponse.statusCode {} else {
+                    let decoder = JSONDecoder()
+                    let apiError = try decoder.decode(APIErrorMessage.self, from: data)
+                    
+                    if urlResponse.statusCode == 400 {
+                        throw APIError.validationError(apiError.reason)
+                    }
+                    
+                    if (500..<600) ~= urlResponse.statusCode {
+                        let retryAfter = urlResponse.value(forHTTPHeaderField: "Retry-After")
+                        throw APIError.serverError(statusCode: urlResponse.statusCode, reason: apiError.reason, retryAfter: retryAfter)
+                    }
+                }
+                return (data, response)
+            }
+
+        return dataTaskPublisher
             .map(\.data)
             .decode(type: UserNameAvailableMessage.self, decoder: JSONDecoder())
             .map(\.isAvailable)

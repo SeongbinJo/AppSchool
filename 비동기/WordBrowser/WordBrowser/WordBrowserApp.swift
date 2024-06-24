a33a530da1f298c3de0f0abb3eb0653ab9b5b0b5
avan31017 <avan31017@gmail.com>
2024-06-24 13:11:05 +0900

    Create WordBrowser

---
+++ 비동기/WordBrowser/WordBrowser/WordBrowserApp.swift
@@ -0,0 +1,17 @@
+//
+//  WordBrowserApp.swift
+//  WordBrowser
+//
+//  Created by 조성빈 on 6/24/24.
+//
+
+import SwiftUI
+
+@main
+struct WordBrowserApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

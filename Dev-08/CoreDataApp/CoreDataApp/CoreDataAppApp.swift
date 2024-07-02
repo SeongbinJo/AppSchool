e6b921c6e8ad9b172fb749c370af7b7270e6d1bd
avan31017 <avan31017@gmail.com>
2024-07-02 13:47:46 +0900

    delete persistent.swift

--- Dev-08/CoreDataApp/CoreDataApp/CoreDataAppApp.swift
+++ Dev-08/CoreDataApp/CoreDataApp/CoreDataAppApp.swift
@@ -9,12 +9,11 @@ import SwiftUI
 
 @main
 struct CoreDataAppApp: App {
-    let persistenceController = PersistenceController.shared
 
     var body: some Scene {
         WindowGroup {
             ContentView()
-                .environment(\.managedObjectContext, persistenceController.container.viewContext)
+//                .environment(\.managedObjectContext, persistenceController.container.viewContext)
         }
     }
 }
--- Dev-08/CoreDataApp/CoreDataApp/CoreDataManager.swift
+++ Dev-08/CoreDataApp/CoreDataApp/CoreDataManager.swift
@@ -11,12 +11,46 @@ import CoreData
 class CoreDataManager {
     let persistentContainer: NSPersistentContainer
     init() {
-        persistentContainer = NSPersistentContainer(name: "CoreDataApp") // 코어데이터의 이름인 CoreDataApp 사용
-        persistentContainer.loadPersistentStores { (description, error) in
+        persistentContainer = NSPersistentContainer(name: "CoreDataApp") // CoreDataApp 이라는 DB(코어데이터)를 초기화
+        persistentContainer.loadPersistentStores { (description, error) in // 초기화된 컨테이너에 데이터들을 load하는 부분. -> 데이터를 가져오는 부분이라 비동기로 처리.
             if let error = error {
                 fatalError("Core Data failed to initialize \(error.localizedDescription)")
             }
-            
+        }
+    }
+    
+    // Create
+    func savePet(name: String, breed: String) {
+        let pet = Animal(context: persistentContainer.viewContext)
+        pet.name = name
+        pet.breed = breed
+        do {
+            try persistentContainer.viewContext.save()
+            print("Pet saved!")
+        } catch {
+            print("Failed to save pet \(error)")
+        }
+    }
+    
+    // Read (List)
+    func getAllPets() -> [Animal] {
+        let fetchRequest: NSFetchRequest<Animal> = Animal.fetchRequest()
+        do {
+            return try persistentContainer.viewContext.fetch(fetchRequest)
+        } catch {
+            return []
+        }
+        
+    }
+    
+    // Delete
+    func deletePet(animal: Animal) {
+        persistentContainer.viewContext.delete(animal)
+        do {
+            try persistentContainer.viewContext.save()
+        } catch {
+            persistentContainer.viewContext.rollback()
+            print("Failed to save context \(error.localizedDescription)")
         }
     }
 }

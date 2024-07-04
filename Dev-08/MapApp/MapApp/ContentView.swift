21bd567ff8dd23181b7d080580f303196b942dc7
avan31017 <avan31017@gmail.com>
2024-07-04 10:08:49 +0900

    Map with Annotation

--- Dev-08/MapApp/MapApp/ContentView.swift
+++ Dev-08/MapApp/MapApp/ContentView.swift
@@ -8,17 +8,33 @@
 import SwiftUI
 import MapKit
 
+extension CLLocationCoordinate2D {
+    static let paris = CLLocationCoordinate2D(latitude: 48.856613, longitude: 2.352222)
+}
+
 struct ContentView: View {
     let locationManager = CLLocationManager()
     
     @State var message = "Map of Paris"
-    @State private var region: MKCoordinateRegion = MKCoordinateRegion(center: CLLocationCoordinate2D(latitude: 48.856613, longitude: 2.352222), span: MKCoordinateSpan(latitudeDelta: 0.05, longitudeDelta: 0.05))
-
+    @State private var region: MKCoordinateRegion = MKCoordinateRegion(center: .paris, span: MKCoordinateSpan(latitudeDelta: 0.05, longitudeDelta: 0.05))
+    @State private var position: MapCameraPosition = .region(MKCoordinateRegion(center: .paris, span: MKCoordinateSpan(latitudeDelta: 0.05, longitudeDelta: 0.05)))
     
     var body: some View {
         VStack {
-            Map(coordinateRegion: $region)
-                .mapStyle(.hybrid)
+            
+            // 기존의 map 사용방식
+//            Map(coordinateRegion: $region)
+//                .mapStyle(.hybrid)
+            
+            // 새로운 map 사용방식
+            Map(position: $position) {
+                Annotation("에펠탑", coordinate: .paris) {
+                    Image(systemName: "mappin.circle.fill")
+                        .foregroundColor(.purple)
+                        .background(.white)
+                        .clipShape(Circle())
+                }
+            }
             TextEditor(text: $message)
                 .frame(width: .infinity, height: 100)
         }

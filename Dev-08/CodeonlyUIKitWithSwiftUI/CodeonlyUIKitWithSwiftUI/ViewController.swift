e666d3ac14984f2e9a7acecac324515dc69d5dd4
avan31017 <avan31017@gmail.com>
2024-07-08 13:33:07 +0900

    create Code only with UIKit + SwiftUI

---
+++ Dev-08/CodeonlyUIKitWithSwiftUI/CodeonlyUIKitWithSwiftUI/ViewController.swift
@@ -0,0 +1,21 @@
+//
+//  ViewController.swift
+//  CodeonlyUIKitWithSwiftUI
+//
+//  Created by 조성빈 on 7/8/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        view.backgroundColor = .white
+        title = "콜드재현"
+    }
+
+
+}
+

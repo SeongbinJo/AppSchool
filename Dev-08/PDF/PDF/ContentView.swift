08c7f73846e4e793e527f0106cd1aa1a0e57ffdc
avan31017 <avan31017@gmail.com>
2024-07-08 11:37:15 +0900

    Create PDF, setup

---
+++ Dev-08/PDF/PDF/ContentView.swift
@@ -0,0 +1,36 @@
+//
+//  ContentView.swift
+//  PDF
+//
+//  Created by 조성빈 on 7/8/24.
+//
+
+import SwiftUI
+import PDFKit
+
+struct ContentView: View {
+    let fileURL = Bundle.main.url(forResource: "example", withExtension: "pdf")
+    var body: some View {
+        VStack {
+            ViewMe(url: fileURL!)
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
+
+struct ViewMe: UIViewRepresentable {
+    let url: URL
+    
+    func makeUIView(context: UIViewRepresentableContext<ViewMe>) -> PDFView {
+        let pdfView = PDFView()
+        pdfView.document = PDFDocument(url: url)
+        return pdfView
+    }
+    
+    func updateUIView(_ uiView: PDFView, context: Context) {
+    }
+}
---
+++ Dev-08/PDF/PDF/PDFApp.swift
@@ -0,0 +1,17 @@
+//
+//  PDFApp.swift
+//  PDF
+//
+//  Created by 조성빈 on 7/8/24.
+//
+
+import SwiftUI
+
+@main
+struct PDFApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

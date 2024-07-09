c0d9f888efec1ba20edd8719cfb6e1ca5f3d43b4
avan31017 <avan31017@gmail.com>
2024-07-09 13:30:06 +0900

    CoreMLImage - View setup

--- Dev-08/CoreMLImage/CoreMLImage/ContentView.swift
+++ Dev-08/CoreMLImage/CoreMLImage/ContentView.swift
@@ -6,14 +6,55 @@
 //
 
 import SwiftUI
+import CoreML
+import Vision
 
 struct ContentView: View {
+    
+    @State var message = ""
+    @State var arrayIndex = 0
+    @State var image: UIImage = UIImage(named: "cat")!
+    
+    let photoArray = ["cat", "plane", "banana", "car"]
+    
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+            Image(uiImage: image)
+                .resizable()
+                .aspectRatio(contentMode: .fit)
+                .frame(width: 250, height: 250)
+            TextEditor(text: $message)
+                .padding()
+            Button(action: {}) {
+                Text("Analyze Image")
+            }
+            
+            HStack {
+                Button {
+                    if arrayIndex == 0 {
+                        arrayIndex = photoArray.count - 1
+                    } else {
+                        arrayIndex -= 1
+                    }
+                    message = ""
+                    image = UIImage(named: photoArray[arrayIndex])!
+                } label: {
+                    Image(systemName: "chevron.left.square.fill")
+                }
+                Button {
+                    if arrayIndex == photoArray.count - 1 {
+                        arrayIndex = 0
+                    } else {
+                        arrayIndex += 1
+                    }
+                    message = ""
+                    image = UIImage(named: photoArray[arrayIndex])!
+                } label: {
+                    Image(systemName: "chevron.right.square.fill")
+                }
+                
+            }
+            .padding()
         }
         .padding()
     }

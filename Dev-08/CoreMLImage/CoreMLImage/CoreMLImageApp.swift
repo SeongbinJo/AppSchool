09f620ac1c8638d14eed65b726cb1bc64cc38bc9
avan31017 <avan31017@gmail.com>
2024-07-09 13:19:01 +0900

    create CoreMLImage

---
+++ Dev-08/CoreMLImage/CoreMLImage/CoreMLImageApp.swift
@@ -0,0 +1,17 @@
+//
+//  CoreMLImageApp.swift
+//  CoreMLImage
+//
+//  Created by 조성빈 on 7/9/24.
+//
+
+import SwiftUI
+
+@main
+struct CoreMLImageApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
